9f9967adbae8f83db80d83d90c6bc9671775910e
anujgupta1368 <97537197+anujgupta1368@users.noreply.github.com>
2023-02-11 22:12:00 +0530

    Add files via upload

---
+++ contactform.js
@@ -0,0 +1,16 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzp-bqsCAkBpL9uSKEnRogYBENuWDl4PP6xYrDrYA_pFb0VEpQT3oRYwUCSNcSVvqaH/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg=document.getElementById("msg");
+
+        form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = "Message sent successfully!"
+                setTimeout(function(){
+                    msg.innerHTML = "";
+                },5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+        })
